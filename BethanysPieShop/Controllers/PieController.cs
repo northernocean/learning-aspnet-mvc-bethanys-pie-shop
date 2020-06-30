b66895b4ae9cb3176996be6f64724b8b1d7ffd17
david <4504352+northernocean@users.noreply.github.com>
2020-06-30 01:01:12 +0000

    Create Pie Controller

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        // GET: /<controller>/
+        public IActionResult List()
+        {
+            PiesListViewModel piesListViewModel = new PiesListViewModel
+            {
+                Pies = _pieRepository.AllPies,
+                CurrentCategory = "Cheese cakes"
+            };
+            return View(piesListViewModel);
+        }
+    }
+}
