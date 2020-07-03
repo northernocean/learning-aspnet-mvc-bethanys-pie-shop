71adacb0ce481b8819a0965ec3f67a4ffa312bd2
david <4504352+northernocean@users.noreply.github.com>
2020-07-03 20:50:50 +0000

    Refactor pies list into a partial view

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -31,5 +31,13 @@ namespace BethanysPieShop.Controllers
             };
             return View(piesListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+            return View(pie);
+        }
     }
 }
