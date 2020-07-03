621e1b985a370ad825e1e6a383ef8269d3a3bb20
david <4504352+northernocean@users.noreply.github.com>
2020-07-03 13:39:26 +0000

    Add Seed Data

--- BethanysPieShop/Models/AppDbContext.cs
+++ BethanysPieShop/Models/AppDbContext.cs
@@ -14,5 +14,13 @@ namespace BethanysPieShop.Models
         public DbSet<Pie> Pies { get; set; }
         public DbSet<Category> Categories { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            SeedData.SeedPies.Seed(modelBuilder);
+            SeedData.SeedCategories.Seed(modelBuilder);
+
+        }
+
     }
 }
---
+++ BethanysPieShop/Models/SeedData/SeedCategories.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShop.Models.SeedData
+{
+    public static class SeedCategories
+    {
+
+        public static void Seed(ModelBuilder modelBuilder)
+        {
+            //seed categories
+            modelBuilder.Entity<Category>().HasData(new Category { CategoryId = 1, CategoryName = "Fruit pies" });
+            modelBuilder.Entity<Category>().HasData(new Category { CategoryId = 2, CategoryName = "Cheese cakes" });
+            modelBuilder.Entity<Category>().HasData(new Category { CategoryId = 3, CategoryName = "Seasonal pies" });
+        }
+
+    }
+}
