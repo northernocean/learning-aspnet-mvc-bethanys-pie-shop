f1ec73f757a75aba2b98a04f033c1270e57e1581
david <4504352+northernocean@users.noreply.github.com>
2020-07-03 13:17:48 +0000

    Add AppDBContext and Repositories

---
+++ BethanysPieShop/Models/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public CategoryRepository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IEnumerable<Category> AllCategories => dbContext.Categories;
+    }
+}
---
+++ BethanysPieShop/Models/PieRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public PieRepository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IEnumerable<Pie> AllPies 
+        {
+            get 
+            {
+                return dbContext.Pies.Include(c => c.Category);
+            }
+        }
+
+        public IEnumerable<Pie> PiesOfTheWeek 
+        {
+            get 
+            {
+                return dbContext.Pies.Include(c => c.Category).Where(p => p.IsPieOfTheWeek);
+            }
+        }
+
+        public IEnumerable<Category> AllCategories
+        {
+            get
+            {
+                return dbContext.Categories;
+            }
+        }
+        
+
+        public Pie GetPieById(int pieId)
+        {
+            return dbContext.Pies.SingleOrDefault(p => p.PieId == pieId);
+        }
+    }
+}
