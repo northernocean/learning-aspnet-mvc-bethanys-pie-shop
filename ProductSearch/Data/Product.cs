ae650f216b6089f9461490568e1a98441db97aac
David Pecot <4504352+northernocean@users.noreply.github.com>
2023-03-19 17:35:07 -0400

    add search for product

---
+++ ProductSearch/Data/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace ProductSearch.Data
+{
+    public class Product
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+
+        public Product(string title, decimal price, string image)
+        {
+            Title = title;
+            Price = price;
+            Image = image;
+        }
+    }
+}
